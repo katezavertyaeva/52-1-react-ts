102f0152265454a1060e8243ffc1560cd01132aa
Ekaterina Zavertyaeva <katsiarynazavertyaeva@gmail.com>
2025-01-30 10:41:29 +0100

    add lesson 11 frontend

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import GlobalStyles from './styles/GlobalStyles'
 // import Lesson07 from './lessons/Lesson07/Lesson07'
 // import Lesson08 from './lessons/Lesson08/Lesson08'
 // import Lesson09 from './lessons/Lesson09/Lesson09'
-import Lesson10 from './lessons/Lesson10/Lesson10'
+// import Lesson10 from './lessons/Lesson10/Lesson10'
 
 //homeworks
 // import Homework08 from './homeworks/Homework08/Homework08'
 // import Homework09 from './homeworks/Homework09/Homework09'
+import Homework10 from './homeworks/Homework10/Homework10'
 
 //consultations
 // import Consultation03 from './consultations/Consultation03/Consultation03'
@@ -34,7 +35,8 @@ function App() {
       {/* <Consultation_04 /> */}
       {/* <Homework09 /> */}
       {/* --Topic: useEffect, axios */}
-      <Lesson10 />
+      {/* <Lesson10 /> */}
+      <Homework10 />
     </>
   )
 }
